e492fc6c968c37cc1d307c0c98f10bb3d0b62301
CGH123 <327048688@qq.com>
2016-04-17 14:43:04 +0800

    Naga framework

---
+++ app/src/main/java/com/example/administrator/puzzleGame/naga/exception/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Exceptions used by Naga.
+ */
+package com.example.administrator.puzzleGame.naga.exception;
---
+++ app/src/main/java/com/example/administrator/puzzleGame/naga/packetreader/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing various ready-to-use {@code PacketReader} implementations.
+ */
+package com.example.administrator.puzzleGame.naga.packetreader;
