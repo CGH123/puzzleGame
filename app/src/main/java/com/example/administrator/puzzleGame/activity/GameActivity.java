03e38c732e6e6c0dbe8f24bb2be83c82f551154a
guojun <gjscut@qq.com>
2016-04-24 23:27:37 +0800

    udp

--- app/src/main/java/com/example/administrator/puzzleGame/activity/GameActivity.java
+++ app/src/main/java/com/example/administrator/puzzleGame/activity/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.puzzleGame.activity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -23,7 +24,7 @@ import com.example.serialization.SerializerFastJson;
 /**
  * Created by HUI on 2016-04-24.
  */
-public class GameActivity extends Activity implements View.OnClickListener{
+public class GameActivity extends Activity implements View.OnClickListener {
 
     Button sent;
     Button server_init;
@@ -34,8 +35,6 @@ public class GameActivity extends Activity implements View.OnClickListener{
     Server server;
     Client client;
 
-    boolean isServer = false;
-    boolean isClient = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +45,15 @@ public class GameActivity extends Activity implements View.OnClickListener{
         initEvent();
     }
 
-    private void initView(){
-        sent = (Button)findViewById(R.id.test_send);
-        server_init = (Button)findViewById(R.id.server_init);
-        client_init = (Button)findViewById(R.id.client_init);
-        content = (TextView)findViewById(R.id.text_show);
+    private void initView() {
+        sent = (Button) findViewById(R.id.test_send);
+        server_init = (Button) findViewById(R.id.server_init);
+        client_init = (Button) findViewById(R.id.client_init);
+        content = (TextView) findViewById(R.id.text_show);
         myHandler = new MyHandler();
     }
 
-    private void initEvent(){
+    private void initEvent() {
         sent.setOnClickListener(this);
         server_init.setOnClickListener(this);
         client_init.setOnClickListener(this);
@@ -65,24 +64,64 @@ public class GameActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.test_send:
-                if(!isClient) break;
-                MSGProtocol msgProtocol = new MSGProtocol("Hi",1);
+                MSGProtocol msgProtocol = new MSGProtocol("Hi", 1);
                 String temp = SerializerFastJson.getInstance().serialize(msgProtocol);
                 client.sendToServer(temp.getBytes());
 
                 Message msg = new Message();
                 msg.what = NetConstant.Message_Sen;
                 Bundle bundle = new Bundle();
-                bundle.putString("name",msgProtocol.getSenderName());
+                bundle.putString("name", msgProtocol.getSenderName());
                 msg.setData(bundle);
                 myHandler.sendMessage(msg);
-
                 break;
             case R.id.server_init:
-                if(isServer) break;
-                isServer = true;
+                new NetStartTask(ServerLAN.getInstance()).execute();
+                break;
+            case R.id.client_init:
+                new NetStartTask(ClientLAN.getInstance()).execute();
+                break;
+        }
+    }
+
+    private class MyHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle;
+            String name;
+            switch (msg.what) {
+                case NetConstant.Message_Rec:
+                    //TODO 处理收到的消息
+                    bundle = msg.getData();
+                    name = "我收到消息" + bundle.getString("name") + "\n";
+                    content.setText(content.getText() + name);
+                    break;
+                case NetConstant.Message_Sen:
+                    //TODO 处理发送的消息
+
+                    bundle = msg.getData();
+                    name = "我发送消息" + bundle.getString("name") + "\n";
+                    content.setText(content.getText() + name);
+                    break;
+            }
+        }
+    }
+
+    class NetStartTask extends AsyncTask<Void,Void,String> {
+        Server server;
+        Client client;
+        NetStartTask(Server server){
+            this.server = server;
+        }
+        NetStartTask(Client client){
+            this.client = client;
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            if(server != null) {
                 OnServerReadListener serverReadListener = new OnServerReadListener() {
                     @Override
                     public void processMsg(byte[] packet, NIOSocket nioSocket) {
@@ -91,22 +130,18 @@ public class GameActivity extends Activity implements View.OnClickListener{
                         Message msg = new Message();
                         msg.what = NetConstant.Message_Rec;
                         Bundle bundle = new Bundle();
-                        bundle.putString("name",msgProtocol.getSenderName());
+                        bundle.putString("name", msgProtocol.getSenderName());
                         msg.setData(bundle);
                         myHandler.sendMessage(msg);
 
                         MSGProtocol temp = new MSGProtocol("hello", msgProtocol.getCommand());
                         String msgString = SerializerFastJson.getInstance().serialize(temp);
-                        //server.sendToClient(msgString.getBytes(),nioSocket);
                         server.sendAllClient(msgString.getBytes());
                     }
                 };
-
                 server.addServerReadListener(serverReadListener).bind(NetConstant.TCP_PORT).start();
-                break;
-            case R.id.client_init:
-                if(isClient) break;
-                isClient = true;
+            }
+            else if(client != null){
                 OnClientReadListener clientReadListener = new OnClientReadListener() {
                     @Override
                     public void processMsg(byte[] packet) {
@@ -115,40 +150,22 @@ public class GameActivity extends Activity implements View.OnClickListener{
                         Message msg = new Message();
                         msg.what = NetConstant.Message_Rec;
                         Bundle bundle = new Bundle();
-                        bundle.putString("name",msgProtocol.getSenderName());
+                        bundle.putString("name", msgProtocol.getSenderName());
                         msg.setData(bundle);
                         myHandler.sendMessage(msg);
 
                     }
                 };
-
-                client.addClientReadListener(clientReadListener).bind(NetConstant.TCP_PORT).start();
-                break;
+                client.addClientReadListener(clientReadListener).bind("localhost", NetConstant.TCP_PORT).start();
+            }
+            return "Success";
         }
-    }
-
-    private class MyHandler extends Handler{
 
         @Override
-        public void handleMessage(Message msg) {
-            Bundle bundle;
-            String name;
-            switch(msg.what){
-                case NetConstant.Message_Rec:
-                    //TODO 处理收到的消息
-                    bundle = msg.getData();
-                    name = "我收到消息"+bundle.getString("name")+"\n";
-                    content.setText(content.getText()+name);
-                    break;
-                case NetConstant.Message_Sen:
-                    //TODO 处理发送的消息
+        protected void onPostExecute(String ip) {
 
-                    bundle = msg.getData();
-                    name = "我发送消息"+bundle.getString("name")+"\n";
-                    content.setText(content.getText()+name);
-                    break;
-            }
         }
+
     }
 
 
